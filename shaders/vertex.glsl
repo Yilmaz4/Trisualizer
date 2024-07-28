c36eed5643cf073b02cf51b1a33dd96d0771520a
Yılmaz Alpaslan <ymzymz2007@gmail.com>
2024-07-28 20:14:58 +0300

    Fix dark edges in low-res graphs

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -16,7 +16,6 @@ uniform bool quad;
 
 out vec3 normal;
 out vec3 fragPos;
-out vec2 gridPos;
 out vec2 gridCoord;
 
 void main() {
@@ -24,23 +23,19 @@ void main() {
 		gl_Position = vec4(aPos, 1.f);
 		return;
 	}
-	const float halfres = grid_res / 2.f;
-	float x = (gl_VertexID % grid_res);
-	float y = floor(gl_VertexID / grid_res);
-	gridPos = vec2((x - halfres) / (grid_res / 10.f), (y - halfres) / (grid_res / 10.f));
-	fragPos = vec3((graph_size / grid_res) * (x - halfres), graph_size * grid[gl_VertexID], (graph_size / grid_res) * (y - halfres));
-	gridCoord = vec2(zoom * (x - halfres) / grid_res, zoom * (y - halfres) / grid_res) + centerPos.xy;
-	
-	vec3 v1, v2, v3, v4;
-	if (x == grid_res) v1 = fragPos;
-	else v1 = vec3((graph_size / grid_res) * (x + 1 - halfres), graph_size * grid[int(y * grid_res + (x + 1.f))], (graph_size / grid_res) * (y - halfres));
-	if (y == grid_res) v2 = fragPos;
-	else v2 = vec3((graph_size / grid_res) * (x - halfres), graph_size * grid[int((y + 1.f) * grid_res + x)], (graph_size / grid_res) * (y + 1 - halfres));
+	const float gridres = grid_res + 2;
+	const float halfres = gridres / 2.f;
+	float x = (gl_VertexID % grid_res) + 1;
+	float y = floor(gl_VertexID / grid_res) + 1;
 
-	if (x == 0.f) v3 = fragPos;
-	else v3 = vec3((graph_size / grid_res) * (x - 1 - halfres), graph_size * grid[int(y * grid_res + (x - 1.f))], (graph_size / grid_res) * (y - halfres));
-	if (y == 0.f) v4 = fragPos;
-	else v4 = vec3((graph_size / grid_res) * (x - halfres), graph_size * grid[int((y - 1.f) * grid_res + x)], (graph_size / grid_res) * (y - 1 - halfres));
+	fragPos = vec3((graph_size / gridres) * (x - halfres), graph_size * grid[int(y * gridres + x)], (graph_size / gridres) * (y - halfres));
+	gridCoord = vec2(zoom * (x - halfres) / gridres, zoom * (y - halfres) / gridres) + centerPos.xy;
+	
+	vec3 v1 = vec3((graph_size / gridres) * (x + 1 - halfres), graph_size * grid[int(y * gridres + (x + 1.f))], (graph_size / gridres) * (y - halfres));
+	vec3 v2 = vec3((graph_size / gridres) * (x - halfres), graph_size * grid[int((y + 1.f) * gridres + x)], (graph_size / gridres) * (y + 1 - halfres));
+
+	vec3 v3 = vec3((graph_size / gridres) * (x - 1 - halfres), graph_size * grid[int(y * gridres + (x - 1.f))], (graph_size / gridres) * (y - halfres));
+	vec3 v4 = vec3((graph_size / gridres) * (x - halfres), graph_size * grid[int((y - 1.f) * gridres + x)], (graph_size / gridres) * (y - 1 - halfres));
 
 	normal = normalize(cross(v1 - fragPos, v2 - fragPos) + cross(v3 - fragPos, v4 - fragPos));
 
